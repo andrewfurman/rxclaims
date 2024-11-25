63770e2d77e427073c388b601ffc12d7c25d327b
Andrew Furman <aifurman@gmail.com>
2024-11-25 22:16:14 +0000

    updated view member

--- members/member_routes.py
+++ members/member_routes.py
@@ -8,3 +8,8 @@ members_bp = Blueprint('members', __name__,
 def members():
     members = Member.query.all()
     return render_template('members/members.html', members=members)
+
+@members_bp.route('/members/<member_id>')
+def view_member(member_id):
+    member = Member.query.filter_by(member_id=member_id).first_or_404()
+    return render_template('members/view_member.html', member=member)
