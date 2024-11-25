087608b8442a22bfda201856a8f93a82772714c6
Andrew Furman <aifurman@gmail.com>
2024-11-25 16:49:37 +0000

    got members working

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from members.member_model import Member, db
 
-app = Flask(__name__)
+app = Flask(__name__, 
+           template_folder='.')  # This allows templates to be found in subfolders
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+db.init_app(app)
 
 @app.route('/')
 def index():
     return 'HELLO RxClaims'
 
+@app.route('/members')
+def members():
+    members = Member.query.all()
+    return render_template('members/members.html', members=members)
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
